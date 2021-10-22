e4f6c6241ebbf55fe854d9191e177f26106176ab
ryanreadbooks <958796758@qq.com>
2021-10-22 18:25:51 +0800

    init

--- convert_gn1b_2_cornell.py
+++ convert_gn1b_2_cornell.py
@@ -29,15 +29,15 @@ def run(args):
         g = GraspNet(graspnet_root, camera=camera, split='all')
         for scene_id in range(scene_start, scene_end):
             for ann_id in range(256):
-                # 读出场景中的抓取矩形框
+                # load rect grasp from graspnet-1b
                 rect_grasps: RectGraspGroup = g.loadGrasp(sceneId=scene_id, camera=camera, annId=ann_id, format='rect')
-                # 将所有抓取矩形框按照分数由高到低进行排序
+                # sort them
                 rect_grasps = rect_grasps.sort_by_score()
-                n = (int) (len(rect_grasps) * 0.05) # 只要前5%的grasp
+                n = (int) (len(rect_grasps) * 0.05) # only use top-5% grasps
                 rect_grasps = rect_grasps[:n]
                 corners = rect_grasps.get_all_rect_corners()    # (n, 8)
                 corners = corners.reshape(-1, 2) # (n * 4, 2)
-                # 写入文件中 两个两个一行
+                # write it into file, 
                 dir = os.path.join(graspnet_root, 'rect_labels_cornell', f'scene_{str(scene_id).zfill(4)}', camera)
                 ensure_dir(dir)
                 saved_path = os.path.join(dir, f'{str(ann_id).zfill(4)}.txt')
