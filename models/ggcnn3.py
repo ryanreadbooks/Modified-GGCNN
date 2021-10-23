8c444b24347b6b00dbd420ea01b52c0222e2b1fd
ryanreadbooks <958796758@qq.com>
2021-10-23 12:20:54 +0800

    add models

---
+++ models/ggcnn3.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class GGCNN3(nn.Module):
+    def __init__(self):
+        super().__init__()
+    
+    def forward(self, x):
+        pass
